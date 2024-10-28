3a71db1fb19c45476693676e317f2ed19c38008e
Andrei Luiz de jesus <159576658+Andrei0769@users.noreply.github.com>
2024-10-28 12:30:22 +0000

    classse main

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from services.usuario_service import UsuarioService
+from repositories.usuario_repository import UsuarioRepository
+from config.database import Session
+
+def main():
+    session = Session()
+    repository = UsuarioRepository(session)
+    service = UsuarioService(repository)
+
+    # SOLICITANDO DADOS PARA O USUARIO.
+    print("\nAdicionando usuários")
+    nome = input("Digite o seu nome:")
+    email = input("Digite o seu e-mail:")
+    senha = input("Digite sua senha: ")
+
+    service.criar_usuario(nome=nome, email=email, senha=senha)
+
+    print("\nListando usuários cadastrados")
+    listar_usuarios = service.listar_todos_usuarios()
+    for usuario in listar_usuarios:
+        print(f"Nome: {usuario.nome} - Email: `{usuario.email} - semha: {usuario.senha}")
+
+if __name__ == "__main__":
+    main():
--- app/services/usuario_service.py
+++ app/services/usuario_service.py
@@ -1,8 +1,8 @@
 from models.usuario_models import Usuario
-from repositories.usuario_repository import UsuarioRepositoy
+from repositories.usuario_repository import UsuarioRepository
 
 class UsuarioService:
-    def __init__(self, repository: UsuarioRepositoy):
+    def __init__(self, repository: UsuarioRepository):
         self.repository = repository
     
     def criar_usuario(self, nome: str, email: str, senha: str):
